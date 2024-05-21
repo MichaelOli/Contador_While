6607273a46264cfa5606779b786dfd70b29e5509
MichaelOli <michaeloliveira38@gmail.com>
2024-05-21 20:59:51 -0300

    Versão 1.2

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 #Usuario informa um número
-while True:
-    try:
-        contador = int(input('Informe um número inteiro: '))
-        contador >= 0
-        contador -=1
-        print(contador)
-        break
-    except ValueError:
-        print('Por favor informe um valor válido!')
+import os
+import time
 
+'''contador = int(input('Informe um número inteiro: '))
+contador -= 1
+while True:
+    try:   
+        print(contador)
+        time.sleep(0.5)
+        
+    except ValueError:
+        print('Por favor informe um valor válido!')'''
 
 # Usuario informa um número
-'''contador = int(input('Informe um número inteiro: '))
+contador = int(input('Informe um número inteiro: '))
+os.system('cls')
 while contador >=0:
-    print(contador)
+    print(f'Contagem regressiva: {contador}...')
+    time.sleep(1)
+    os.system('cls')
     contador -= 1
-'''
+
+print('Booooom!!!')
